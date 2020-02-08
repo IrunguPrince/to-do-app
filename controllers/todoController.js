3aac4f4e8438f6c1c5cf0bf82685bb9135d46bf2
Prince Irungu <princeirungu@gmail.com>
2020-02-08 12:25:01 +0300

    Connected to mongodb and added data dynamically and retrieved to the view dynamically

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,8 +2,11 @@ var bodyParser = require('body-parser');
 var mongoose =  require('mongoose');
 
 //connect to DB
-mongoose.connect('mongodb+srv://prince:prince3253@cluster0-anlwk.mongodb.net/test?retryWrites=true&w=majority');
-
+mongoose.connect('mongodb+srv://prince:prince3253@cluster0-anlwk.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true });
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 //create schema(like a blueprint)
 var todoSchema  = new mongoose.Schema({
     item: String
@@ -11,27 +14,38 @@ var todoSchema  = new mongoose.Schema({
 
 //create model
 var Todo = mongoose.model('Todo', todoSchema);
-var itemOne = Todo({item: 'buy car'}).save(function(err){
-    if(err) throw err;
-    console.log('Item saved successfully');
-});
-var data = [{item: 'read a book'},{item: 'listen to some music'},{item: 'get some goals'}];
+
+//var data = [{item: 'read a book'},{item: 'listen to some music'},{item: 'get some goals'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
     app.get('/todo', function(req, res){
-        res.render('todo',{todos: data});
+        //get data from mongodb and pass it to the view
+        Todo.find({},function(err, data){
+            if(err) throw err;
+            res.render('todo',{todos: data});
+        }); 
     });
     app.post('/todo', urlencodedParser, function(req, res){
-        data.push(req.body);
-        res.json(data);
+        //get data from view and add it to the database
+        var newTodo = Todo(req.body).save(function(err, data){
+            if(err) throw err;
+            res.json(data);
+        });
     });
 
     app.delete('/todo/:item', function(req, res){
-        data = data.filter(function(todo){
+        //delete requested item from mongodb
+        Todo.findOneAndRemove({item: req.params.item.replace(/\-/g, "")},function(err, data){
+            if(err) throw err;
+            res.json(data);
+        });
+           
+        
+       /* data = data.filter(function(todo){
             return todo.item.replace(/ /g,'-') !== req.params.item;
         });
-        res.json(data);
+        res.json(data);*/
     });
 
 };
